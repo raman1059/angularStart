a42b2b92577dd4c2996642b5b3859b5d8f65f6f1
Ramanpreet Singh <ramanpreet1059@gmail.com>
2019-03-17 16:47:08 +0530

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
